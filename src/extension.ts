8051aeb14ab2ddc4bae4150a220924f2a2187019
Douglas Waugh <douglas.waugh.e3@gmail.com>
2023-04-11 20:36:23 +0200

    Tries to copy file contents to a new file

--- src/extension.ts
+++ src/extension.ts
@@ -1,29 +1,67 @@
 import * as vscode from 'vscode';
+import * as path from 'path';
 
-export function activate(context: vscode.ExtensionContext) {
-	console.log('Congratulations, your extension "Renamer" is now active!');
+async function customSaveAs() {
+  const activeEditor = vscode.window.activeTextEditor;
+  if (!activeEditor) {
+    return;
+  }
 
-  let disposable = vscode.workspace.onWillSaveTextDocument((e: vscode.TextDocumentWillSaveEvent) => {
-		console.log('onWillSaveTextDocument fires');
+  const document = activeEditor.document;
+  const defaultUri = document.isUntitled
+    ? (vscode.workspace.workspaceFolders?.[0]?.uri ?? vscode.Uri.file(''))
+    : document.uri;
 
-    const document = e.document;
-    const originalUri = document.uri;
+  let fileName = path.basename(defaultUri.fsPath);
+  const newFileName = fileName.toLowerCase().split(' ').join('-');
 
-    if (originalUri.scheme === 'file') {
-      const fileName = originalUri.fsPath.split(/[\\/]/).pop();
-      
-      if (fileName) {
-        const newFileName = fileName.toLowerCase().split(' ').join('-');
-        
-        if (fileName !== newFileName) {
-          const newUri = vscode.Uri.file(originalUri.fsPath.replace(fileName, newFileName));
-          e.waitUntil(vscode.workspace.fs.rename(originalUri, newUri));
-        }
-      }
-    }
-  });
+  if (fileName !== newFileName) {
+    fileName = newFileName;
 
-  context.subscriptions.push(disposable);
+    const newDocument = await vscode.workspace.openTextDocument({
+      language: document.languageId,
+      content: document.getText(),
+    });
+
+    await vscode.window.showTextDocument(newDocument);
+  }
+
+  const options: vscode.SaveDialogOptions = {
+    defaultUri: defaultUri.with({ path: path.join(path.dirname(defaultUri.path), fileName) }),
+    filters: {
+      'All Files': ['*'],
+    },
+  };
+
+  const newUri = await vscode.window.showSaveDialog(options);
+  if (newUri) {
+    // Save the new document using the provided URI
+    const newDocument = await vscode.workspace.openTextDocument(newUri);
+    await newDocument.save();
+  }
 }
 
-export function deactivate() {}
+async function copyFile(fileUri:vscode.Uri, newUri:vscode.Uri)
+{
+  if (fileUri.scheme !== 'file') {
+    vscode.window.showErrorMessage('Cannot copy a non-file document');
+    return;
+  }
+
+  const fileContent = new Uint8Array(await vscode.workspace.fs.readFile(fileUri));
+  const fileDir = path.dirname(fileUri.fsPath);
+  const newFileUri = vscode.Uri.file(path.join(fileDir, newUri.toString()));
+
+  // Create a new file with the copied content
+  await vscode.workspace.fs.writeFile(newFileUri, fileContent);
+}
+
+export function activate(context: vscode.ExtensionContext) {
+  // Register the custom "Save As" command
+  console.log('Congratulations, your extension "Renamer" is now active!');
+  context.subscriptions.push(
+    vscode.commands.registerCommand('renamer.customSaveAs', customSaveAs)
+  );
+
+  // The rest of your activate function code
+}
